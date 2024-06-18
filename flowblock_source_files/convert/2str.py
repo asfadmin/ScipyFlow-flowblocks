c67505f1a9605b3afb9ac27c5dbac06fd38152bd
segallagher <segallagher@alaska.edu>
2024-06-17 16:53:46 -0800

    chore: add debug prints

--- flowblock_source_files/convert/2str.py
+++ flowblock_source_files/convert/2str.py
@@ -10,7 +10,9 @@ description: "Attempts to convert the input to a string"
 """
 
 def main(input):
+    print(f"2str input {input}")
     try:
         return str(input)
     except ValueError:
+        print(f"Error converting to str")
         raise ValueError
