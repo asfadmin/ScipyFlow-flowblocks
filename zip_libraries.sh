408fc8272adb8dd4989928ee78332961b9638896
segallagher <segallagher@alaska.edu>
2024-07-10 10:36:57 -0800

    fix: fixes Removing a flowblock and rezipping libraries does not delete removed flowblock #14

--- zip_libraries.sh
+++ zip_libraries.sh
@@ -4,5 +4,8 @@
 #list all flowblock libraries into library_list.txt
 ls -l ./flowblock_source_files/ | grep '^d' | awk '{$1=$2=$3=$4=$5=$6=$7=$8=""; print substr($0, 9)}' > ./flowblock_source_files/library_list.txt
 
+#delete all zipped libraries
+cat ./flowblock_source_files/library_list.txt | xargs -I {} bash -c 'rm ./flowblock_source_files/{}/*.zip'
+
 #zip all library contents
 cat ./flowblock_source_files/library_list.txt | xargs -I {} bash -c 'zip -j flowblock_source_files/{}/library.zip ./flowblock_source_files/{}/*.py'
