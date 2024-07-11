96160b3be444b5649f47be1f61a3181c457b6d57
segallagher <segallagher@alaska.edu>
2024-07-10 16:26:32 -0800

    chore: attempt printing with print

--- flowblock_source_files/base/debug_print.py
+++ flowblock_source_files/base/debug_print.py
@@ -10,5 +10,6 @@ description: "Prints input string to web console"
 import pyodide.code
 
 def main(input_string):
+    print("Python print")
     js_code = f'console.log({input_string});'
     pyodide.code.run_js(js_code)
