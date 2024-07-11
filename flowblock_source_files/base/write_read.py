bd98907896a0b4411f211665619eed5537f9e481
segallagher <segallagher@alaska.edu>
2024-07-11 13:19:10 -0800

    feat: add Write/Read file flowblock

---
+++ flowblock_source_files/base/write_read.py
@@ -0,0 +1,26 @@
+"""
+name: "Write/Read file"
+inputs:
+    path:
+        type: Str
+        user_input: True
+    write_mode:
+        type: Str
+        user_input: True
+        default: False
+    write_contents:
+        type: Str
+        user_input: True
+        default: ""
+outputs:
+    string:
+        type: Str
+description: "Writes or reads to a path depending on write_mode"
+"""
+from pathlib import Path
+
+def main(path, write_mode, write_contents):
+    if write_mode:
+        Path(path).write_text(write_contents)
+        return path
+    return Path(path).read_text()
