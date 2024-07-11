c04eff32bf357d16538fcf84ce8b55f53d694215
segallagher <segallagher@alaska.edu>
2024-07-10 16:12:47 -0800

    chore: attempt running arbitrary js code

---
+++ flowblock_source_files/base/debug_print.py
@@ -0,0 +1,14 @@
+"""
+name: "Debug Print"
+inputs:
+    input_string:
+        type: Str
+        user_input: True
+description: "Prints input string to web console"
+"""
+
+import pyodide.code
+
+def main(input_string):
+    js_code = f'console.log({input_string});'
+    pyodide.code.run_js(js_code)
