52b75700e47d581285d78522f0b9ada4f5762c8f
segallagher <segallagher@alaska.edu>
2024-07-12 14:28:03 -0800

    refactor: save input image as bytes before setting image source

--- flowblock_source_files/base/output_image.py
+++ flowblock_source_files/base/output_image.py
@@ -8,6 +8,14 @@ description: "Writes the input PIL image to the \"Image Output\" field."
 """
 
 from js import document
+from io import BytesIO
+import base64
 
 def main(image):
-    document.getElementById("img").src = image
+
+    # Return image as data
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    data = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+    document.getElementById("img").src = f"data:image/png;base64,{{data}}"
