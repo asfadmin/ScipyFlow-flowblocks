c4598526cfd9afc946dc707a45ef81ea6e01bec2
segallagher <segallagher@alaska.edu>
2024-07-17 13:52:09 -0800

    chore: add aa flowblock

---
+++ flowblock_source_files/base/aa.py
@@ -0,0 +1,22 @@
+"""
+name: "a"
+requirements:
+inputs:
+    string1:
+        type: Str
+        default: "Python"
+        user_input: True
+
+    string2:
+        type: Str
+        default: " exists"
+        user_input: True
+outputs:
+    concatenated_string:
+        type: Str
+description: "Returns the concatenation of two input strings."
+"""
+
+# adding a comment
+def main(string1, string2):
+    return string1 + string2
