c1346391e00e9f9fa80b883c9d8c601c64e81bf0
segallagher <segallagher@alaska.edu>
2024-07-24 10:24:57 -0800

    feat: add weird flowblock

---
+++ flowblock_source_files/experimental/weird.py
@@ -0,0 +1,20 @@
+"""
+name: "Weird"
+requirements:
+inputs:
+    string1:
+        type: Str
+        default: "Gurble"
+        user_input: True
+
+    string2:
+        type: Str
+        user_input: True
+outputs:
+    concatenated_string:
+        type: Str
+description: "Does things"
+"""
+
+def main(string1, string2):
+    return string1 + string2 + " is a thing"
