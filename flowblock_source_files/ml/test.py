ee54ac2554328221c7f4ff941ed572a53ea247a3
segallagher <segallagher@alaska.edu>
2024-08-08 15:32:29 -0800

    feat: attempt importing sklearn

---
+++ flowblock_source_files/ml/test.py
@@ -0,0 +1,17 @@
+"""
+name: "Test ML"
+requirements:
+    - ultralytics
+    - pandas
+    - scikit-learn
+inputs:
+outputs:
+description: "Returns the sum of a and b"
+"""
+
+import pandas
+from sklearn.datasets import load_iris
+import ultralytics
+
+def main():
+    pass
