442bf681928bf5667eb50a2bafbd42d2912b5bf3
segallagher <segallagher@alaska.edu>
2024-08-09 15:41:21 -0800

    fix: wrap main code in main() function

--- flowblock_source_files/ml/semantic_segmentation.py
+++ flowblock_source_files/ml/semantic_segmentation.py
@@ -80,38 +80,40 @@ def mse_prime(y_true, y_pred):
     return 2 * (y_pred - y_true) / np.size(y_true)
 
 
+def main():
+    # XOR solution
+    X = np.reshape([[0,0],[0,1],[1,0],[1,1]], (4,2,1))
+    Y = np.reshape([[0],[1],[1],[0]],(4,1,1))
 
-# XOR solution
-X = np.reshape([[0,0],[0,1],[1,0],[1,1]], (4,2,1))
-Y = np.reshape([[0],[1],[1],[0]],(4,1,1))
+    network = [
+        Dense(2,3),
+        Tanh(),
+        Dense(3,1),
+        Tanh(),
+    ]
 
-network = [
-    Dense(2,3),
-    Tanh(),
-    Dense(3,1),
-    Tanh(),
-]
+    epochs = 1000
+    learning_rate = 0.1
 
-epochs = 1000
-learning_rate = 0.1
+    #train
+    for e in range(epochs):
+        error = 0
+        for x,y in zip(X,Y):
+            # forward
+            output = x
+            for layer in network:
+                output = layer.forward(output)
 
-#train
-for e in range(epochs):
-    error = 0
-    for x,y in zip(X,Y):
-        # forward
-        output = x
-        for layer in network:
-            output = layer.forward(output)
+            # error
+            error += mse(y,output)
 
-        # error
-        error += mse(y,output)
+            # backward
+            grad = mse_prime(y, output)
+            logging.info("GRADIENT:", type(grad))
+            for layer in reversed(network):
+                grad = layer.backward(grad, learning_rate)
+        
+        error /= len(X)
+        logging.info('%d/%d, error=%f' % (e + 1, epochs, error))
 
-        # backward
-        grad = mse_prime(y, output)
-        logging.info("GRADIENT:", type(grad))
-        for layer in reversed(network):
-            grad = layer.backward(grad, learning_rate)
-    
-    error /= len(X)
-    logging.info('%d/%d, error=%f' % (e + 1, epochs, error))
+    return "Done"
