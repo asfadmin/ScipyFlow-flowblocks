e1e75b91477c2fd6b175152ed7444bb63b7e3e34
segallagher <segallagher@alaska.edu>
2024-08-09 15:59:43 -0800

    chore: replace type info with printouts of variables

--- flowblock_source_files/ml/semantic_segmentation.py
+++ flowblock_source_files/ml/semantic_segmentation.py
@@ -43,11 +43,11 @@ class Dense(Layer):
         return np.dot(self.weights, self.input) + self.bias
     
     def backward(self, output_gradient, learning_rate):
-        logging.info(f"self.intut: {self.input}")
+        logging.info(f"self.input: {self.input}")
         weights_gradient = np.dot(output_gradient, self.input.T)
-        logging.info(f"learning rate: {type(learning_rate)}\nweights_gradient: {type(weights_gradient)}")
+        logging.info(f"learning rate: {learning_rate}\nweights_gradient: {weights_gradient}")
         self.weights -= learning_rate * weights_gradient
-        logging.info(f"learning rate: {type(learning_rate)}\noutput_gradient: {type(output_gradient)}")
+        logging.info(f"learning rate: {learning_rate}\noutput_gradient: {output_gradient}")
         self.bias -= learning_rate * output_gradient
         return np.dot(self.weights.T, output_gradient)
     
