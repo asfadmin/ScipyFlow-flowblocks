3ceadd933b802b63b94387468c1943fa7f01de21
segallagher <segallagher@alaska.edu>
2024-08-09 15:37:06 -0800

    chore: switch all debug logging to info

--- flowblock_source_files/ml/semantic_segmentation.py
+++ flowblock_source_files/ml/semantic_segmentation.py
@@ -44,9 +44,9 @@ class Dense(Layer):
     
     def backward(self, output_gradient, learning_rate):
         weights_gradient = np.dot(output_gradient, self.input.T)
-        logging.debug("learning rate:", type(learning_rate), "\nweights_gradient:", type(weights_gradient))
+        logging.info("learning rate:", type(learning_rate), "\nweights_gradient:", type(weights_gradient))
         self.weights -= learning_rate * weights_gradient
-        logging.debug("learning rate:", type(learning_rate), "\noutput_gradient:", type(output_gradient))
+        logging.info("learning rate:", type(learning_rate), "\noutput_gradient:", type(output_gradient))
         self.bias -= learning_rate * output_gradient
         return np.dot(self.weights.T, output_gradient)
     
@@ -109,7 +109,7 @@ for e in range(epochs):
 
         # backward
         grad = mse_prime(y, output)
-        logging.debug("GRADIENT:", type(grad))
+        logging.info("GRADIENT:", type(grad))
         for layer in reversed(network):
             grad = layer.backward(grad, learning_rate)
     
