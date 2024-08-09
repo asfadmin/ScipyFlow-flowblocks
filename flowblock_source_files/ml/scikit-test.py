57b76edd8ca6d2b287aa45cde4f07c28f7a994b4
segallagher <segallagher@alaska.edu>
2024-08-09 12:16:34 -0800

    chore: only import  scikit-learn

--- flowblock_source_files/ml/scikit-test.py
+++ flowblock_source_files/ml/scikit-test.py
@@ -1,19 +1,18 @@
 """
 name: "Test ML"
 requirements:
-    - pandas
-    - numpy
-    - scipy
-# inputs:
+    - scikit-learn
+inputs:
 outputs:
 description: "Returns the sum of a and b"
 """
 # - scikit-learn
     # - ultralytics
 
-import pandas
-# from sklearn.datasets import load_iris
+# import pandas
+from sklearn.datasets import load_iris
 # import ultralytics
 
 def main():
+    print("Python ran")
     pass
