c279f2329570d49cd54c68e3e6abd61e5c7cd56e
segallagher <segallagher@alaska.edu>
2024-08-09 15:05:23 -0800

    fix: mse syntax error

--- flowblock_source_files/ml/semantic_segmentation.py
+++ flowblock_source_files/ml/semantic_segmentation.py
@@ -70,7 +70,7 @@ class Tanh(Activation):
 
 # Loss functions
 def mse(y_true, y_pred):
-    return np.mean(np.power(y_true - y_pred), 2)
+    return np.mean(np.power(y_true - y_pred, 2))
 
 def mse_prime(y_true, y_pred):
     return 2 * (y_pred - y_true) / np.size(y_true)
