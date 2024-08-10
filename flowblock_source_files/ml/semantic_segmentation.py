e5d434fd1879e5dc5ff00a7a82d4a3cc8392dfe7
segallagher <segallagher@alaska.edu>
2024-08-09 16:56:22 -0800

    feat: implement convolution

--- flowblock_source_files/ml/semantic_segmentation.py
+++ flowblock_source_files/ml/semantic_segmentation.py
@@ -3,6 +3,7 @@ name: "Semantic Segmentation"
 requirements:
     - pandas
     - numpy
+    - scipy
     - scikit-learn
 inputs:
 outputs:
@@ -12,6 +13,7 @@ description: "A test flowblock to show semantic segmentation in pyodide"
 """
 
 import pandas
+from scipy import signal
 import numpy as np
 import logging
 
@@ -48,9 +50,48 @@ class Dense(Layer):
         self.bias -= learning_rate * output_gradient
         return np.dot(self.weights.T, output_gradient)
     
-class Convolutional():
-    pass
+class Convolutional(Layer):
+    def __init__(self, input_shape, kernel_size, depth):
+        input_depth, input_height, input_width = input_shape
+        self.depth = depth
+        self.input_shape = input_shape
+        self.input_depth = input_depth
+        self.output_shape = (depth, input_height - kernel_size + 1, input_width - kernel_size + 1)
+        self.kernels_shape = (depth, input_depth, kernel_size, kernel_size)
+        self.kernels = np.random.randn(*self.kernels_shape)
+        self.biases = np.random.randn(*self.output_shape)
 
+    def forward(self, input):
+        self.input = input
+        self.output = np.copy(self.biases)
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                self.output[i] += signal.correlate2d(self.input[j], self.kernels[i, j], "valid")
+        return self.output
+    
+    def backward(self, output_gradient, learning_rate):
+        kernels_gradient = np.zeros(self.kernels_shape)
+        input_gradient = np.zeros(self.input_shape)
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                kernels_gradient[i, j] = signal.correlate2d(self.input[j], output_gradient[i], "valid")
+                input_gradient[j] += signal.convolve2d(output_gradient[i], self.kernels[i, j], "full")
+
+        self.kernels -= learning_rate * kernels_gradient
+        self.biases -= learning_rate * output_gradient
+        return input_gradient
+    
+class Reshape(Layer):
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+    def forward(self, input):
+        return np.reshape(input, self.output_shape)
+    
+    def backward(self, output_gradient, learning_rate):
+        return np.reshape(output_gradient, self.input_shape)
+    
 # Activation functions
 class Activation(Layer):
     def __init__(self, activation, activation_prime):
@@ -70,14 +111,33 @@ class Tanh(Activation):
         tanh_prime = lambda x: 1 - np.tanh(x) ** 2
         super().__init__(tanh, tanh_prime)
 
-# Loss functions
+class Sigmoid(Activation):
+    def __init__(self):
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+        
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+        
+        super().__init__(sigmoid, sigmoid_prime)
+
+## Loss functions
+# MSE
 def mse(y_true, y_pred):
     return np.mean(np.power(y_true - y_pred, 2))
 
 def mse_prime(y_true, y_pred):
     return 2 * (y_pred - y_true) / np.size(y_true)
 
+# binary_cross_entropy
+def binary_cross_entropy(y_true, y_pred):
+    return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
 
+def binary_cross_entropy_prime(y_true, y_pred):
+    return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
+
+# Execute code
 def main():
     # XOR solution
     X = np.reshape([[0,0],[0,1],[1,0],[1,1]], (4,2,1))
