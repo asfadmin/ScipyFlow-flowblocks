87d4398f071f18411514e637d5de5853a9db461f
segallagher <segallagher@alaska.edu>
2024-10-26 14:28:21 -0800

    fix: remove unused output, made paths os independent, comment cleanup

--- flowblock_source_files/base/download_file.py
+++ flowblock_source_files/base/download_file.py
@@ -14,39 +14,30 @@ inputs:
 outputs:
     path:
         type: Str
-    success:
-        type: Bool
 description: "Downloads a file to the specified path"
 """
 
 import requests
 import os
-from pathlib import Path
 
 def main(URL, directory):
-    # create output path
     file_name = os.path.split(URL)[1]
-    if directory == "/":
-        path = f'/{file_name}'
-    else:
-        path = f'{directory}/{file_name}'
+    
+    # create output path    
+    output_path = os.path.join(directory, file_name)
 
     # get file from URL
     resp = requests.get(URL)
-    print(f'resp code: {resp.status_code}')
-    # print(f'resp text: {resp.content}')   # Memory error
+    print(f"Request to {URL} status code: {resp.status_code}")
     if resp.status_code != 200:
-        return False
+        return None
 
     # create path if it does not exist
-    x = os.path.exists(os.path.dirname(path))
-    print(f"does path exist:{x}")
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
+    x = os.path.exists(os.path.dirname(output_path))
+    if not os.path.exists(os.path.dirname(output_path)):
+        os.makedirs(os.path.dirname(output_path))
 
     # write file
-    with open(path, 'wb') as f:
-        print("Writing to file")
+    with open(output_path, 'wb') as f:
         f.write(resp.content)
-    print(f'File saved to {path}')
-    return path, True
+    return output_path
